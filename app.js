108459b9f709aa1ecf30812ab9cf76743a7ef573
liusenlong <Admin!@#123>
2026-05-18 22:36:28 +0800

    fix： share

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ App({
     if (token) {
       this.globalData.token = token;
     }
+  },
+  onShareAppMessage() {
+    return {
+      title: 'Excel数据处理与PDF工具',
+      path: '/pages/home/home',
+      imageUrl: ''
+    };
   }
 });
